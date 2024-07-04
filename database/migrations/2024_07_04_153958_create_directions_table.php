b5d49ba3c105101075b743cd2f0ee6bd60987308
Roxandesanz <sanchezrandrade@gmail.com>
2024-07-04 09:59:39 -0600

    Creación de mograciones
    - Students
    - Teachers
    - Directions

---
+++ database/migrations/2024_07_04_153958_create_directions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('directions', function (Blueprint $table) {
+            $table->id();
+            $table->string('line_one', 50);
+            $table->string('line_two', 50)->nullable();
+            $table->integer('postal_code');
+            $table->string('state', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('directions');
+    }
+};
